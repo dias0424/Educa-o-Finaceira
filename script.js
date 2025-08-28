ec02d563eb9b2cc42bab5b3bef18fe40f275bfc7
dias0424 <pedro.dias.moreira@escola.pr.gov.br>
2025-08-28 11:09:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+const botao = document.querySelector('.boto');
+
+let contador = 0;
+
+function adicionarClique() {
+    contador++; // Incrementa o contador
+    botao.textContent = `Número de cliques: ${contador}`; // Atualiza o texto do botão
+}
+
+botao.addEventListener('click', adicionarClique);
+
